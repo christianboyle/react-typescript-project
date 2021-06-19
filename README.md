bea13e9169f0a8d234e122ffb0d74d865aa0bce6
Christian Boyle <mail@christianboyle.com>
2021-06-19 12:52:58 -0500

    🎉

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# react-typescript-starter
-minimal react/ts/parcel starter for quick experimentation
+# clem-react-coding-interview-intermediate
+
+Code from Clément Mihailescu's intermediate coding interview with Ben Awad: https://www.youtube.com/watch?v=6s0OVdoo4Q4
 
 # get started
+
 ```
 npm i
 npm run dev
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from "react-dom";
 
-const { useState } = React;
+import App from "./App";
 
-export default function App() {
-  const [counter, setCounter] = useState(0);
-
-  return (
-    <div className="App">
-      <h1>My React and TypeScript App! {counter}</h1>
-      <button
-        onClick={() => {
-          setCounter(counter + 1);
-        }}
-      >
-        Increase Counter
-      </button>
-    </div>
-  );
-}
-
-ReactDOM.render(<App />, document.getElementById('root'));
+const rootElement = document.getElementById("root");
+render(<App />, rootElement);
